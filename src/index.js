9eda5054b15a3a08739e7af760518bb80fea436d
AadiNir <96436873+AadiNir@users.noreply.github.com>
2023-08-28 12:27:52 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,148 @@
+import contractABI from "./abi.json";
+const contractAddress = "0xACF912BB2F9006a83a0E48E3997BF6b7F4ac50bD";
+
+let web3 = new Web3(window.ethereum);
+
+let contract = new web3.eth.Contract(contractABI, contractAddress);
+
+async function connectWallet() {
+  if (window.ethereum) {
+    const accounts = await window.ethereum
+      .request({ method: "eth_requestAccounts" })
+      .catch((err) => {
+        if (err.code === 4001) {
+          console.log("Please connect to MetaMask.");
+        } else {
+          console.error(err);
+        }
+      });
+    console.log(accounts);
+    setConnected(accounts[0]);
+  } else {
+    console.error("No web3 provider detected");
+    document.getElementById("connectMessage").innerText =
+      "No web3 provider detected. Please install MetaMask.";
+  }
+}
+
+async function createTweet(content) {
+  const accounts = await web3.eth.getAccounts();
+  try {
+    await contract.methods.createtweets(content).send({ from: accounts[0] });
+    displayTweets(accounts[0]);
+  } catch (error) {
+    console.error("User rejected request:", error);
+  }
+}
+
+async function displayTweets(userAddress) {
+  const tweetsContainer = document.getElementById("tweetsContainer");
+  tweetsContainer.innerHTML = "";
+
+  const tempTweets = await contract.methods.alltweets(userAddress).call();
+  // we do this so we can sort the tweets  by timestamp
+  const tweets = [...tempTweets];
+  tweets.sort((a, b) => b.timestamp - a.timestamp);
+  for (let i = 0; i < tweets.length; i++) {
+    const tweetElement = document.createElement("div");
+    tweetElement.className = "tweet";
+
+    const userIcon = document.createElement("img");
+    userIcon.className = "user-icon";
+    userIcon.src = `https://avatars.dicebear.com/api/human/${tweets[i].author}.svg`;
+    userIcon.alt = "User Icon";
+
+    tweetElement.appendChild(userIcon);
+
+    const tweetInner = document.createElement("div");
+    tweetInner.className = "tweet-inner";
+
+    tweetInner.innerHTML += `
+        <div class="author">${shortAddress(tweets[i].author)}</div>
+        <div class="content">${tweets[i].content}</div>
+    `;
+
+    const likeButton = document.createElement("button");
+    likeButton.className = "like-button";
+    likeButton.innerHTML = `
+        <i class="far fa-heart"></i>
+        <span class="likes-count">${tweets[i].likes}</span>
+    `;
+    likeButton.setAttribute("data-id", tweets[i].id);
+    likeButton.setAttribute("data-author", tweets[i].author);
+
+    addLikeButtonListener(
+      likeButton,
+      userAddress,
+      tweets[i].id,
+      tweets[i].author
+    );
+    tweetInner.appendChild(likeButton);
+    tweetElement.appendChild(tweetInner);
+
+    tweetsContainer.appendChild(tweetElement);
+  }
+}
+
+function addLikeButtonListener(likeButton, address, id, author) {
+  likeButton.addEventListener("click", async (e) => {
+    e.preventDefault();
+
+    e.currentTarget.innerHTML = '<div class="spinner"></div>';
+    e.currentTarget.disabled = true;
+    try {
+      await likeTweet(author, id);
+      displayTweets(address);
+    } catch (error) {
+      console.error("Error liking tweet:", error);
+    }
+  });
+}
+
+function shortAddress(address, startLength = 6, endLength = 4) {
+  return `${address.slice(0, startLength)}...${address.slice(-endLength)}`;
+}
+
+async function likeTweet(author, id) {
+  const accounts = await web3.eth.getAccounts();
+  try {
+    // 8️⃣ call the likeTweet function from smart contract
+    // INPUT: author and id
+    // GOAL: Save the like in the smart contract
+    // HINT: don't forget to use await 😉 👇
+    await contract.methods.liketweet(id, author).send({ from: accounts[0] });
+  } catch (error) {
+    console.error("User rejected request:", error);
+  }
+}
+
+function setConnected(address) {
+  document.getElementById("userAddress").innerText =
+    "Connected: " + shortAddress(address);
+  document.getElementById("connectMessage").style.display = "none";
+  document.getElementById("tweetForm").style.display = "block";
+  displayTweets(address);
+  // This is the function in the javascript code, not smart contract 😉
+  // GOAL: display all tweets after connecting to metamask
+}
+
+document
+  .getElementById("connectWalletBtn")
+  .addEventListener("click", connectWallet);
+
+document.getElementById("tweetForm").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const content = document.getElementById("tweetContent").value;
+  const tweetSubmitButton = document.getElementById("tweetSubmitBtn");
+  tweetSubmitButton.innerHTML = '<div class="spinner"></div>';
+  tweetSubmitButton.disabled = true;
+  try {
+    await createTweet(content);
+  } catch (error) {
+    console.error("Error sending tweet:", error);
+  } finally {
+    // Restore the original button text
+    tweetSubmitButton.innerHTML = "Tweet";
+    tweetSubmitButton.disabled = false;
+  }
+});
